96e2c23985051c3cbf9c0a8a6718630430a1fa28
Chris Martin <chrisjmartin03@gmail.com>
2020-09-14 13:52:27 +0100

    continuing refactoring notebook - combining the headline and lsoa data, and producing some initial plots.

--- functions_importing_cases.R
+++ functions_importing_cases.R
@@ -6,3 +6,12 @@ test_function <- function(x){
 num_distinct_values <- function(df, var_name){
   n_distinct(df[var_name])
 }
+
+get_cases_per_week <- function(df, case_col, na.rm = TRUE, ...){
+  ## ... used to pass additional variable names for grouping
+  case_col <- enquo(case_col)
+  
+  df %>%
+    group_by(week_commencing, ...) %>%
+    summarise(cases_per_week = sum(!!case_col, na.rm = na.rm))
+}
