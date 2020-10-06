f887486bfaa4cec6ba5ed02d888d0cf0703006c9
Chris Martin <chrisjmartin03@gmail.com>
2020-10-06 20:09:17 +0100

    Starting to work with the data.gov coronavirus API

---
+++ gov_uk_covid_r_sdk.R
@@ -0,0 +1,57 @@
+# from https://coronavirus.data.gov.uk/developers-guide on 6th October 2010
+
+#' Extracts paginated data by requesting all of the pages
+#' and combining the results.
+#'
+#' @param filters    API filters. See the API documentations for 
+#'                   additional information.
+#'                   
+#' @param structure  Structure parameter. See the API documentations 
+#'                   for additional information.
+#'                   
+#' @return list      Comprehensive list of dictionaries containing all 
+#'                   the data for the given ``filter`` and ``structure`.`
+library(httr)
+
+get_paginated_data <- function (filters, structure) {
+  
+  endpoint     <- "https://api.coronavirus.data.gov.uk/v1/data"
+  results      <- list()
+  current_page <- 1
+  
+  repeat {
+    
+    httr::GET(
+      url   = endpoint,
+      query = list(
+        filters   = paste(filters, collapse = ";"),
+        structure = jsonlite::toJSON(structure, auto_unbox = TRUE),
+        page      = current_page
+      ),
+      timeout(50)
+    ) -> response
+    
+    # Handle errors:
+    if ( response$status_code >= 400 ) {
+      err_msg = httr::http_status(response)
+      stop(err_msg)
+    } else if ( response$status_code == 204 ) {
+      break
+    }
+    
+    # Convert response from binary to JSON:
+    json_text <- content(response, "text")
+    dt        <- jsonlite::fromJSON(json_text)
+    results   <- rbind(results, dt$data)
+    
+    if ( is.null( dt$pagination$`next` ) ){
+      break
+    }
+    
+    current_page <- current_page + 1;
+    
+  }
+  
+  return(results)
+  
+}
